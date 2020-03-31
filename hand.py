0c788b2109cdd8fadf0a3ac23686a3477482362e
Patrick Riley <patriley@gmail.com>
2020-03-30 22:00:15 -0700

    5 card hand ranking

--- hand.py
+++ hand.py
@@ -44,4 +44,98 @@ class Hand:
         Raises:
           ValueError: if the hand has less than 5 cards
         """
-        return [HIGH_CARD]
+        if len(self.cards) != 5:
+            raise ValueError("Not enough cards ({}) in {} to get hand rank"
+                             .format(len(self.cards), self.cards))
+
+        # To make finding the low straights easier, we'll keep two
+        # versions of our rank counts, one with the low ace included
+        # and one without.
+        rank_counts = [0] * 15
+        rank_counts_low_ace = [0] * 15
+        for c in self.cards:
+            rank_counts[c.rank()] += 1
+            rank_counts_low_ace[c.rank()] += 1
+            if c.rank() == 14:
+                rank_counts_low_ace[1] += 1
+
+        # We'll need this for straight and for straight flush, so
+        # we'll calculate it here.
+        straight_high_rank = None
+        num_straight_cards = 0
+        for rank, count in enumerate(rank_counts_low_ace):
+            if count == 1:
+                num_straight_cards += 1
+                if num_straight_cards >= 5:
+                    straight_high_rank = rank
+            elif count == 0:
+                num_straight_cards = 0
+
+        # The list of single cards is needed to fill out the ranking
+        # array for several hands so we'll just do it here.
+        sorted_singles = list(reversed([
+                r for r, count in enumerate(rank_counts) if count == 1]))
+        
+                
+        suit_counts = [0] * 4
+        for c in self.cards:
+            suit_counts[c.suit().value] += 1
+
+        # straight flush
+        try:
+            suit_counts.index(5)
+            if straight_high_rank:
+                return [HandRank.STRAIGHT_FLUSH, straight_high_rank]
+        except ValueError:
+            pass
+            
+        # four of a kind
+        try:
+            four_of_a_kind_rank = rank_counts.index(4)
+            other_rank = rank_counts.index(1)
+            return [HandRank.FOUR_OF_A_KIND, four_of_a_kind_rank, other_rank]
+        except ValueError:
+            pass
+
+        # full house
+        try:
+            high_rank = rank_counts.index(3)
+            low_rank = rank_counts.index(2)
+            return [HandRank.FULL_HOUSE, high_rank, low_rank]
+        except ValueError:
+            pass
+
+        # flush
+        try:
+            suit_counts.index(5)
+            return [HandRank.FLUSH] + sorted_singles
+        except ValueError:
+            pass
+
+        # straight
+        if straight_high_rank:
+            return [HandRank.STRAIGHT, straight_high_rank]
+
+        # three of a kind
+        try:
+            rank = rank_counts.index(3)
+            return [HandRank.THREE_OF_A_KIND, rank] + sorted_singles
+        except ValueError:
+            pass
+
+        # two of a kind
+        try:
+            low_rank = rank_counts.index(2)
+            high_rank = rank_counts[low_rank + 1:].index(2) + low_rank + 1
+            return [HandRank.TWO_PAIR, high_rank, low_rank] + sorted_singles
+        except ValueError:
+            pass
+
+        # one pair
+        try:
+            rank = rank_counts.index(2)
+            return [HandRank.ONE_PAIR, rank] + sorted_singles
+        except ValueError:
+            pass
+
+        return [HandRank.HIGH_CARD] + sorted_singles
--- test_hand.py
+++ test_hand.py
@@ -17,8 +17,56 @@ class HandTestCase(unittest.TestCase):
                          h.cards)
         self.assertEqual("2s 3d 4h", str(h))
 
+    def test_hand_rank_too_few_cards(self):
+        with self.assertRaises(ValueError):
+            hand.Hand.from_str("2s 3c").hand_rank()
+        
     def test_hand_rank_5_card(self):
-        pass
+        # for straight flushes. check that we check the high and
+        # the low.
+        self.assertEqual([hand.HandRank.STRAIGHT_FLUSH, 10],
+                         hand.Hand.from_str("9s Ts 8s 7s 6s").hand_rank())
+        self.assertEqual([hand.HandRank.STRAIGHT_FLUSH, 14],
+                         hand.Hand.from_str("Js Ts Qs Ks As").hand_rank())
+        self.assertEqual([hand.HandRank.STRAIGHT_FLUSH, 5],
+                         hand.Hand.from_str("4s 2s As 3s 5s").hand_rank())
 
+        self.assertEqual([hand.HandRank.FOUR_OF_A_KIND, 8, 14],
+                         hand.Hand.from_str("8s 8c 8d As 8h").hand_rank())
+        # Makes sure we don't call four of a kind a low hand
+        self.assertEqual([hand.HandRank.FOUR_OF_A_KIND, 14, 8],
+                         hand.Hand.from_str("Ac Ad Ah As 8h").hand_rank())
+
+        self.assertEqual([hand.HandRank.FULL_HOUSE, 2, 3],
+                         hand.Hand.from_str("2s 2c 3d 3h 2h").hand_rank())
+        self.assertEqual([hand.HandRank.FULL_HOUSE, 3, 14],
+                         hand.Hand.from_str("As Ac 3d 3h 3h").hand_rank())
+
+        self.assertEqual([hand.HandRank.FLUSH, 14, 12, 10, 8, 6],
+                         hand.Hand.from_str("As Ts 8s 6s Qs").hand_rank())
+
+        # for straights, check that we get the high and the low
+        self.assertEqual([hand.HandRank.STRAIGHT, 10],
+                         hand.Hand.from_str("9c Td 8s 7s 6s").hand_rank())
+        self.assertEqual([hand.HandRank.STRAIGHT, 14],
+                         hand.Hand.from_str("Jc Td Qs Ks As").hand_rank())
+        self.assertEqual([hand.HandRank.STRAIGHT, 5],
+                         hand.Hand.from_str("5c 4d 3s 2s As").hand_rank())
+
+        self.assertEqual([hand.HandRank.THREE_OF_A_KIND, 10, 9, 8],
+                         hand.Hand.from_str("9s Ts Td Tc 8s").hand_rank())
+
+        self.assertEqual([hand.HandRank.TWO_PAIR, 11, 9, 12],
+                         hand.Hand.from_str("9s 9c Js Jd Qc").hand_rank())
+
+        self.assertEqual([hand.HandRank.ONE_PAIR, 3, 7, 6, 5],
+                         hand.Hand.from_str("3s 3c 5c 6h 7s").hand_rank())
+
+        self.assertEqual([hand.HandRank.HIGH_CARD, 10, 8, 6, 4, 2],
+                         hand.Hand.from_str("2s 4c 6d, 8s Ts").hand_rank())
+
+        
+
+    
 if __name__ == '__main__':
     unittest.main()
