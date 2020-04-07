6ae2adafcb09612ffb4441677c02edd77d888f80
Patrick Riley <patriley@gmail.com>
2020-04-06 22:10:25 -0700

    Added basic listen/notify functionality to game.py

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 """Game manages the poker game and the state of the players."""
 
+import enum
+
 MAX_PLAYERS = 10
 
 class GameFullError(Exception):
@@ -21,14 +23,70 @@ class Player:
         self.stack = stack
         self.position = None
 
+    def __str__(self):
+        return "Player({}, {}, {})".format(
+            self.name, self.stack, self.position)
+        
+@enum.unique
+class EventType(enum.Enum):
+    PLAYER_ADDED = 0
+    PLAYER_REMOVED = 1        
+
+class Event:
+    """Events are for communicating out what has happened.
+
+    In order to keep a clean interface between the Manager and the
+    parts of the system that need to respond to changes in the game,
+    objects can subscribe as listeners in a Manager and be passed
+    Event objects telling them all the important things in the game.
+    
+    Attributes:
+      event_type: the type of event (one of the enum EventType)
+      args: a dictionary with the arguments for this event. The expected contents
+            are documented below
+
+    Expected arguments by type:
+      PLAYER_ADDED: player(of type Player)
+      PLAYER_REMOVED: player(of type Player)
+    """
+
+    
+    def __init__(self, event_type, **kwargs):
+        self.event_type = event_type
+        self.args = kwargs
+
+    def __str__(self):
+        args_strs = []
+        for key in sorted(self.args.keys()):
+            args_strs.append("{}={}".format(key, self.args[key]))
+        return "Event({}, {})".format(self.event_type, ", ".join(args_strs))
+
         
 class Manager:
-    """Manager is the main class for managing the flow of the game."""
+    """Manager is the main class for managing the flow of the game.
+
+    See the Event documention for a discussion of the listener setup.
+
+    Note that the notication should happen as the last possible thing in the
+    various acitivities. The listeners may want to access the state of the Manager
+    in response to a notificaiton, so the Manager needs to be in a clean state.
+    """
 
     def __init__(self):
         self.players = [None] * MAX_PLAYERS
+        self.listeners = []
 
+    def add_listener(self, listener):
+        """Adds an Event listener.
+        
+        Args:
+          listener: The only requirement on listener is that it has a notify
+                    method that accepts one positional Event argument.
+        """
+        self.listeners.append(listener)
+        
     def add_player(self, player):
+
         """Add a player to the game
 
         Args:
@@ -45,6 +103,7 @@ class Manager:
                 continue
             player.position = idx
             self.players[idx] = player
+            self._notify(Event(EventType.PLAYER_ADDED, player=player))
             return idx
         
         raise GameFullError()
@@ -68,4 +127,23 @@ class Manager:
         
         removed = self.players[player_idx]
         self.players[player_idx] = None
+
+        self._notify(Event(EventType.PLAYER_REMOVED, player=removed))
+        
         return removed
+
+    def _notify(self, event):
+        for listener in self.listeners:
+            listener.notify(event)
+
+
+class RecordingListener:
+    """A simple listener that just records all events."""
+    def __init__(self):
+        self.events = []
+
+    def clear(self):
+        self.events.clear()
+        
+    def notify(self, event):
+        self.events.append(event)
--- test_game.py
+++ test_game.py
@@ -4,37 +4,54 @@ import game
 
 
 class ManagerTestCase(unittest.TestCase):
+    def setUp(self):
+        self.manager = game.Manager()
+        self.recorder = game.RecordingListener()
+        self.manager.add_listener(self.recorder)
+    
     def test_add_player(self):
-        manager = game.Manager()
         player = game.Player("myname", 1000)
-        self.assertEqual(0, manager.add_player(player))
-        self.assertEqual("myname", manager.players[0].name)
-        self.assertEqual(1000, manager.players[0].stack)
-        self.assertEqual(0, manager.players[0].position)
+        self.assertEqual(0, self.manager.add_player(player))
+        self.assertEqual("myname", self.manager.players[0].name)
+        self.assertEqual(1000, self.manager.players[0].stack)
+        self.assertEqual(0, self.manager.players[0].position)
+
+        self.assertEqual(1, len(self.recorder.events))
+        self.assertEqual(
+            "Event(EventType.PLAYER_ADDED, player=Player(myname, 1000, 0))",
+            str(self.recorder.events[0]))
 
     def test_add_player_too_many(self):
-        manager = game.Manager()
         for idx in range(game.MAX_PLAYERS):
             player = game.Player("name{}".format(idx), 1000 * idx)
-            self.assertEqual(idx, manager.add_player(player))
+            self.assertEqual(idx, self.manager.add_player(player))
         with self.assertRaises(game.GameFullError):
-            manager.add_player(game.Player("full", 12345))
+            self.manager.add_player(game.Player("full", 12345))
 
     def test_remove_player(self):
-        manager = game.Manager()
-        manager.add_player(game.Player("name0", 0))
-        manager.add_player(game.Player("name1", 1000))
-        manager.add_player(game.Player("name2", 2000))
-        removed = manager.remove_player(1)
+        self.manager.add_player(game.Player("name0", 0))
+        self.manager.add_player(game.Player("name1", 1000))
+        self.manager.add_player(game.Player("name2", 2000))
+
+        # We clear the recorder here because we don't care what
+        # happened before the thing we wanted to test.
+        self.recorder.clear()
+        
+        removed = self.manager.remove_player(1)
         self.assertEqual("name1", removed.name)
         self.assertEqual(1000, removed.stack)
         self.assertEqual(1, removed.position)
 
+        self.assertEqual(1, len(self.recorder.events))
+        self.assertEqual(
+            "Event(EventType.PLAYER_REMOVED, player=Player(name1, 1000, 1))",
+            str(self.recorder.events[0]))
+        
         with self.assertRaises(game.NoSuchPlayerError):
-            manager.remove_player(4)
+            self.manager.remove_player(4)
 
         with self.assertRaises(game.NoSuchPlayerError):
-            manager.remove_player(9999)
+            self.manager.remove_player(9999)
 
         
 
